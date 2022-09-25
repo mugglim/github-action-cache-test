1fa6164404352312c62a08c1b0394a079759e592
jaeuk lim <applewodnr@naver.com>
2022-09-26 01:21:03 +0900

    feat: update test code

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,9 +1,19 @@
 const sum = require('./sum');
 
-test('1 + 1 = 2', () => {
-  const x = 1;
-  const y = 2;
-  const result = sum(x, y);
+describe('sum(x,y) = x+y', () => {
+  test('1 + 1 = 2', () => {
+    const x = 1;
+    const y = 2;
+    const result = sum(x, y);
 
-  expect(result).toBe(3);
+    expect(result).toBe(3);
+  });
+
+  test('2 + 3 = 5', () => {
+    const x = 2;
+    const y = 3;
+    const result = sum(2, 3);
+
+    expect(result).toBe(5);
+  });
 });
