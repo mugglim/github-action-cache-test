9133b0934365d2f16438af3bbb1f33235815585d
jaeuk lim <applewodnr@naver.com>
2022-09-27 19:20:10 +0900

    test: add test case

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,17 +3,18 @@ const sum = require('./sum');
 describe('sum(x,y) = x+y', () => {
   test('1 + 1 = 2', () => {
     const x = 1;
-    const y = 2;
-    const result = sum(x, y);
-
-    expect(result).toBe(3);
+    const y = 1;
+    expect(sum(x, y)).toBe(2);
   });
 
   test('2 + 3 = 5', () => {
-    const x = 2;
-    const y = 3;
-    const result = sum(2, 3);
+    expect(sum(2, 3)).toBe(5);
+  });
 
-    expect(result).toBe(5);
+  test('3 + 5 = 8', () => {
+    const x = 3;
+    const y = 5;
+
+    expect(sum(x, y)).toBe(8);
   });
 });
